fc96a7101c1faaeceb08f4f8c0d95acbb23345f8
danielle <danielle@stackery.io>
2019-11-13 12:04:13 -0800

    frontend connect/disconnect

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+      super(props);
+
+      this.state = {
+          ws: null
+      };
+  }
+
+  // single websocket instance for the own application and constantly trying to reconnect.
+
+  componentDidMount() {
+      this.connect();
+  }
+
+  timeout = 250; // Initial timeout duration as a class variable
+
+  /**
+   * @function connect
+   * This function establishes the connect with the websocket and also ensures constant reconnection if connection closes
+   */
+  connect = () => {
+      var ws = new WebSocket("wss://p5i9fz3adl.execute-api.us-west-2.amazonaws.com/development");
+      let that = this; // cache the this
+      var connectInterval;
+
+      // websocket onopen event listener
+      ws.onopen = () => {
+          console.log("connected websocket main component");
+
+          this.setState({ ws: ws });
+
+          that.timeout = 250; // reset timer to 250 on open of websocket connection
+          clearTimeout(connectInterval); // clear Interval on on open of websocket connection
+      };
+
+      // websocket onclose event listener
+      ws.onclose = e => {
+          console.log(
+              `Socket is closed. Reconnect will be attempted in ${Math.min(
+                  10000 / 1000,
+                  (that.timeout + that.timeout) / 1000
+              )} second.`,
+              e.reason
+          );
+
+          that.timeout = that.timeout + that.timeout; //increment retry interval
+          connectInterval = setTimeout(this.check, Math.min(10000, that.timeout)); //call check function after timeout
+      };
+
+      // websocket onerror event listener
+      ws.onerror = err => {
+          console.error(
+              "Socket encountered error: ",
+              err.message,
+              "Closing socket"
+          );
+
+          ws.close();
+      };
+  };
+
+  /**
+   * utilited by the @function connect to check if the connection is close, if so attempts to reconnect
+   */
+  check = () => {
+      const { ws } = this.state;
+      if (!ws || ws.readyState === WebSocket.CLOSED) this.connect(); //check if websocket instance is closed, if so call `connect` function.
+  };
+
+  render() {
+      return (
+        <div>Check the console</div>
+      );
+  }
 }
 
 export default App;
