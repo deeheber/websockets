87b19b32e580730b0322bd01d5d3d6f213923f46
danielle <danielle@stackery.io>
2019-11-13 11:14:51 -0800

    onconnect and ondisconnect function base

--- src/onconnect/index.js
+++ src/onconnect/index.js
@@ -1,6 +1,35 @@
-exports.handler = async (event, context) => {
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  let statusCode;
+  let response;
+
+  try {
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        id: event.requestContext.connectionId,
+        timestamp: Date.now()
+      }
+    };
+
+    console.log(`Adding ID ${event.requestContext.connectionId} to table ${process.env.TABLE_NAME}`);
+    await dynamodb.put(params).promise();
+    console.log('SUCCESS ADDING TO TABLE');
+
+    statusCode = 200;
+    response = `${params} connected`;
+  } catch (err) {
+    statusCode = err.statusCode || 500;
+    response = err.message;
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify(response)
+  };
 };
--- src/ondisconnect/index.js
+++ src/ondisconnect/index.js
@@ -1,6 +1,34 @@
-exports.handler = async (event, context) => {
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async event => {
   // Log the event argument for debugging and for use in local development.
   console.log(JSON.stringify(event, undefined, 2));
 
-  return {};
+  let statusCode;
+  let response;
+
+  try {
+    const params = {
+      TableName: process.env.TABLE_NAME,
+      Key: {
+        id: event.requestContext.connectionId
+      }
+    };
+
+    console.log(`Deleting ID ${event.requestContext.connectionId} from table ${process.env.TABLE_NAME}`);
+    await dynamodb.delete(params).promise();
+    console.log('SUCCESS DELETING FROM TABLE');
+
+    statusCode = 200;
+    response = `${params} disconnected`;
+  } catch (err) {
+    statusCode = err.statusCode || 500;
+    response = err.message;
+  }
+
+  return {
+    statusCode,
+    body: JSON.stringify(response)
+  };
 };
