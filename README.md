5e6ec8a767264f4d6b67bfc53817b12ba2b43ba9
danielle <danielle@stackery.io>
2019-11-14 16:38:40 -0800

    Minor cleanup and docs

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-# Websocket Test
+# Websocket Example
 
-AWS example app https://github.com/aws-samples/simple-websockets-chat-app
-https://blog.bitlabstudio.com/a-simple-chat-app-with-react-node-and-websocket-35d3c9835807
-https://aws.amazon.com/blogs/apn/building-a-simple-serverless-websocket-with-stackery-and-aws/
+## Why Websockets???
+- Websockets are helpful when you need to have two way direction between an application and client
+- Use case in this example: receiving a web notification when an item is removed or put into an s3 bucket
+- Very useful when making an http call that takes a while to process for example using AWS Polly or AWS translate
 
-Frontend
-Add the following to `src/frontend/src/config.js`
+## Directions to Run
+1. Deploy the app. It's recommended to use [Stackery](https://www.stackery.io/)
+2. Add the following to `src/frontend/src/config.js`
+  ```
+  export default {
+    connection: < WEBSOCKET URL HERE STARTING WITH wss: >
+  };
+  ```
+3. `cd src/frontend/src` && `npm install` && `npm start`
+4. Open browser console
+5. Add and remove items from the s3 bucket via the AWS console and see the messages in your browser console
 
-```
-export default {
-  connection: < WEBSOCKET URL HERE STARTING WITH wss: >
-};
-```
+## Additional resources
+- [AWS example app](https://github.com/aws-samples/simple-websockets-chat-app)
+- [Building a serverless Websocket server with Stackery](https://aws.amazon.com/blogs/apn/building-a-simple-serverless-websocket-with-stackery-and-aws/)
+- [React and websocket example](https://blog.bitlabstudio.com/a-simple-chat-app-with-react-node-and-websocket-35d3c9835807)
--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,92 +2,64 @@ import React, { Component } from 'react';
 import config from './config';
 
 class App extends Component {
-  constructor(props) {
-      super(props);
-
-      this.state = {
-          ws: null
-      };
-  }
-
-  // single websocket instance for the own application and constantly trying to reconnect.
-
-  componentDidMount() {
-      this.connect();
-  }
-
-  timeout = 250; // Initial timeout duration as a class variable
-
-  /**
-   * @function connect
-   * This function establishes the connect with the websocket and also ensures constant reconnection if connection closes
-   */
-  connect = () => {
-      var ws = new WebSocket(config.connection);
-      let that = this; // cache the this
-      var connectInterval;
-
-      // websocket onopen event listener
-      ws.onopen = () => {
-          console.log("connected websocket main component");
-
-          this.setState({ ws: ws });
-
-          that.timeout = 250; // reset timer to 250 on open of websocket connection
-          clearTimeout(connectInterval); // clear Interval on on open of websocket connection
-      };
-
-      ws.onmessage = e => {
-        console.log("WebSocket message received:", e);
-      };
-
-      // websocket onclose event listener
-      ws.onclose = e => {
-          console.log(
-              `Socket is closed. Reconnect will be attempted in ${Math.min(
-                  10000 / 1000,
-                  (that.timeout + that.timeout) / 1000
-              )} second.`,
-              e.reason
-          );
-
-          that.timeout = that.timeout + that.timeout; //increment retry interval
-          connectInterval = setTimeout(this.check, Math.min(10000, that.timeout)); //call check function after timeout
-      };
-
-      // websocket onerror event listener
-      ws.onerror = err => {
-          console.error(
-              "Socket encountered error: ",
-              err.message,
-              "Closing socket"
-          );
-
-          ws.close();
-      };
+  state = {
+    ws: new WebSocket(config.connection)
   };
 
-  /**
-   * utilited by the @function connect to check if the connection is close, if so attempts to reconnect
-   */
-  check = () => {
-      const { ws } = this.state;
-      if (!ws || ws.readyState === WebSocket.CLOSED) this.connect(); //check if websocket instance is closed, if so call `connect` function.
+  componentDidMount() {
+    this.connect();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.ws !== this.state.ws) {
+      console.log('new websocket initialized');
+      this.connect();
+    }
+  }
+
+  connect = () => {
+    const { ws } = this.state;
+
+    ws.onopen = () => {
+        console.log('websocket connected');
+    };
+
+    ws.onmessage = event => {
+      console.log('webSocket message received');
+      console.log(event.data);
+    };
+
+    ws.onclose = event => {
+      console.log('onclose event called: websocket disconnected');
+      console.log(event);
+      this.setState({
+        ws: new WebSocket(config.connection)
+      });
+    };
+
+    ws.onerror = err => {
+      console.log('onerror event called: closing websocket');
+      console.log(err);
+      ws.close();
+    };
   };
 
   sendMessage = () => {
     const { ws } = this.state;
     console.log('Sending message...');
-    ws.send(JSON.stringify({ action: 'sendmessage', data: 'Hello world' }));
+    // Update the message here
+    // Ideally would be dynamic from a form or something
+    const message = { action: 'sendmessage', data: 'Hello world' };
+    ws.send(JSON.stringify(message));
   };
 
   render() {
-      return (
-      <div>
-          <h1>Check the console</h1>
-          <button onClick={this.sendMessage}>Send message</button>
-        </div>
-      );
+    return (
+    <div>
+        <h1>Check the console</h1>
+        <button onClick={this.sendMessage}>Send message</button>
+      </div>
+    );
   }
 }
 
